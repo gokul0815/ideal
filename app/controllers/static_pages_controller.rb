d7df069d8a25d84b4e98a8357af279bc08fbd39b
simposk <simposk@gmail.com>
2016-10-01 12:20:05 +0300

    style faq page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,4 @@ class StaticPagesController < ApplicationController
 
   def faqs
   end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   resources :products
 
   root 'static_pages#index'
-
   get 'static_pages/about'
   get 'static_pages/faqs'
-  get 'static_pages/contact'
-
 end
